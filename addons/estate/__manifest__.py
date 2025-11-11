744f29b181f6bda5fcf7b285d903c9a0d3ef715b
maimaliha21 <maimaliha8@gmail.com>
2025-11-11 16:05:47 +0200

    first try odoo adodon and appeard with one example fake data xml

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Mai's Try",
+    'version': '18.0.1.0.0',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'application': True,
+    'demo': ['demo/demo.xml'],
+    'data': ['security/res_groups.xml',
+    'security/ir.model.access.csv',
+    'views/estate_property_views.xml',
+    ],
+}
